6ac2b0e9bd4eb088d6671bbba52cd16093510dad
Konstantin Kononenko <kalikrit@gmail.com>
2017-12-22 01:03:27 +0300

    data update, pie added

--- begoman_chzh/js/main.js
+++ begoman_chzh/js/main.js
@@ -1,9 +1,80 @@
 const TB = document.getElementById("team_bar");
+const TP = document.getElementById("team_pie");
 
 Chart.defaults.global.animation.duration = 3000;
 Chart.defaults.scale.ticks.beginAtZero = true;
 
 let ch_data = [
+	{
+		"date":"21 декабря",
+		"chm_name":"Феофан Дега",
+		"km":36
+	},
+	{
+		"date":"21 декабря",
+		"chm_name":"Ирина Масанова",
+		"km":89
+	},
+	{
+		"date":"21 декабря",
+		"chm_name":"Татьяна Ермохина",
+		"km":44
+	},
+	{
+		"date":"21 декабря",
+		"chm_name":"Макс Лапаев",
+		"km":89
+	},
+	{
+		"date":"21 декабря",
+		"chm_name":"Олег Молчанов",
+		"km":5
+	},
+	{
+		"date":"21 декабря",
+		"chm_name":"Дарья Губарева",
+		"km":7.3
+	},
+	{
+		"date":"21 декабря",
+		"chm_name":"Brown Eyes (Надия)",
+		"km":10
+	},
+	{
+		"date":"21 декабря",
+		"chm_name":"Дмитрий Тожокин",
+		"km":5
+	},
+	{
+		"date":"21 декабря",
+		"chm_name":"Оксана Тихоненков",
+		"km":5.6
+	},
+	{
+		"date":"21 декабря",
+		"chm_name":"Светлана Слепых",
+		"km":4.07
+	},
+	{
+		"date":"21 декабря",
+		"chm_name":"Дина Шостак",
+		"km":5.66
+	},
+	{
+		"date":"21 декабря",
+		"chm_name":"Татьяна Алехина",
+		"km":13.47
+	},
+	{
+		"date":"21 декабря",
+		"chm_name":"Ольга Сандакова",
+		"km":7.72
+	},
+	{
+		"date":"21 декабря",
+		"chm_name":"Анна Дмитриева",
+		"km":7.02
+	},
 	{
 		"date":"21 декабря",
 		"chm_name":"Надежда Зотова",
@@ -102,7 +173,7 @@ let ch_data = [
 	{
 		"date":"21 декабря",
 		"chm_name":"Леонид Дмитриев",
-		"km":5.03
+		"km":26.03
 	}
 ];
 
@@ -452,26 +523,30 @@ let team_members = [
 	}	
 ];
 
-let colors = ["#51574a", "#447c69", "#74c493","#8e8c6d", "#e4bf80", "#e9d78e", "#e2975d", "#f19670", "#e16552","#c94a53", "#be5168", "#a34974","#993767", "#65387d", "#4e2472",        "#9163b6", "#e279a3", "#e0598b","#7c9fb0", "#5698c4", "#9abf88"];
+let colors = ["#51574a", "#447c69", "#74c493","#8e8c6d", "#e4bf80", "#e9d78e", "#e2975d", "#f19670", "#e16552","#c94a53", "#be5168", "#a34974","#993767", "#65387d", "#4e2472", "#9163b6", "#e279a3", "#e0598b","#7c9fb0", "#5698c4", "#9abf88"];
 let labels = ["21 декабря","22 декабря","23 декабря","24 декабря","25 декабря","26 декабря","27 декабря"];
-let data = [];
-let i = 0;
-teams.forEach(function(team){
-	td = {};
-	td.label = team.tn;
-	td.fill = true;
-	td.backgroundColor = colors[i];
-	td.borderColor = "#74c400";
-	i++;
-	td.data = [0];
-	ch_data.forEach(function(cd){
-		if(is_inteam(cd.chm_name, team.alias)){
-			td.data[0] += cd.km;
-		}
+
+function makeBarData(){
+	let bardata = [];
+	let i = 0;
+	teams.forEach(function(team){
+		td = {};
+		td.label = team.tn;
+		td.fill = true;
+		td.backgroundColor = colors[i];
+		td.borderColor = "#74c400";
+		i++;
+		td.data = [0];
+		ch_data.forEach(function(cd){
+			if(is_inteam(cd.chm_name, team.alias)){
+				td.data[0] += cd.km;
+			}
+		});
+		td.data[0] = Math.round(td.data[0]);
+		bardata.push(td);
 	});
-	td.data[0] = Math.round(td.data[0]);
-	data.push(td);
-});
+	return bardata;
+};
 
 function is_inteam(name, team_alias){
 	let found = false;
@@ -486,7 +561,7 @@ let teambar_chart = new Chart(TB, {
     type: 'bar',
 	data: {
 		labels: labels,
-		datasets: data,
+		datasets: makeBarData(),
 		options: {
 			title: {
 			  display: true,
@@ -505,3 +580,43 @@ let teambar_chart = new Chart(TB, {
 	}
 });
 
+let pielabels = [];
+teams.forEach(function(team){
+	pielabels.push(team.tn);
+});
+
+function makePieData(){
+	let piedata = {};
+	piedata.data = [];
+	piedata.backgroundColor = [];
+	piedata.hoverBackgroundColor = [];
+	let i = 0;
+	teams.forEach(function(team){
+		piedata.backgroundColor[i] = colors[i];
+		piedata.hoverBackgroundColor[i] = colors[i];
+		ch_data.forEach(function(cd){
+			if(is_inteam(cd.chm_name, team.alias))
+				piedata.data[i] += cd.km;
+		});
+		piedata.data[i] = Math.round(piedata.data[i]);
+		i++;
+	});
+	let result = [];
+	result.push(piedata);
+	return result;
+};
+
+let teampie_chart = new Chart(TP, {
+	type: 'doughnut',
+	data: {
+		labels: pielabels,
+		datasets: [
+			{
+				data: [89, 10, 44, 70, 44, 43, 61, 74, 89, 36],
+				backgroundColor: ["#51574a", "#447c69", "#74c493", "#8e8c6d", "#e4bf80", "#e9d78e", "#e2975d", "#f19670", "#e16552", "#c94a53"],
+                hoverBackgroundColor: ["#515700", "#447c00", "#74c400", "#8e8c00", "#e4bf00", "#e9d700", "#e29700", "#f19600", "#e16500", "#c94a00"]
+			}
+				],
+		options: {}
+	}	
+});
